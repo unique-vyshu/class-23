926cbff92cdb106adfab80cd230864698bf886d5
unique-vyshu <74426792+unique-vyshu@users.noreply.github.com>
2021-03-06 20:00:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+  
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, ball;
+var box1;
+var box2;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new box(240,30,50,50);
+    box2 =new box(260,60,50,100);
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+   
+    console.log(ground);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+    box1.display();
+    box2.display();
+
+   
+    //rect(ball.position.x, ball.position.y, 20, 20);
+}
+
